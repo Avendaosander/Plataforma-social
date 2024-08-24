24f5a68c27a7a763d710b07a3ce055970354ff1c
Alexander Avendaño <avendano.ramirez@gmail.com>
2024-08-24 16:47:28 -0400

    Update server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,103 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs } from './graphql/typeDefs.js';
-import { resolvers } from './graphql/resolvers/index.js';
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import path from "path"
+import jwt from "jsonwebtoken"
+import { fileURLToPath } from "url"
+import { ApolloServer } from "@apollo/server"
+import { createServer } from "http"
+import { expressMiddleware } from "@apollo/server/express4"
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+import { PrismaClient } from "@prisma/client"  
+import { WebSocketServer } from 'ws' ;   
+import { useServer } from 'graphql-ws/lib/use/ws' ; 
+import { typeDefs } from "./graphql/typeDefs.js"
+import { resolvers } from "./graphql/resolvers/index.js"
+import { insertTechs } from "./helpers/insertData.js"
+import { Context, Token } from "./types.js"
+import router from "./routes/uploads.js"
+insertTechs()
+
+const prisma = new PrismaClient();
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const PORT = process.env.PORT || 4005
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express()
-const PORT = process.env.PORT || 4005;
+const httpServer = createServer(app);
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
 });
 
-const main = async() => {
-  await server.start()
-  
-  app.use(express.json())
-  app.use(morgan('dev'))
-  app.use(cors())
+const serverCleanup = useServer({ schema }, wsServer);
 
-  app.use('/graphql', expressMiddleware(server))
-}
-
-main().then(()=>{
-  app.listen(PORT, ()=>{console.log(`Server running in port http://localhost:${PORT}`)})
+const server = new ApolloServer<Context>({
+	schema,
+	csrfPrevention: false,
+	plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+	formatError: error => {
+		// Registro del error en la consola o archivo de registro (log file)
+		console.error("Error en Apollo Server: ", error)
+		return error
+	},
+	introspection: true,
 })
 
+const main = async () => {
+	await server.start()
 
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: true }))
+	app.use(morgan("dev"))
+	app.use(cors())
+
+	app.use("/uploads", router)
+	app.use(
+		"/graphql",
+		expressMiddleware(server, {
+			context: async ({ req }) => {
+				const { authorization } = req.headers
+				const accessToken = authorization ? authorization.split(' ')[1] : null
+				
+				if (accessToken) {
+					const decoded = jwt.verify(accessToken, process.env.SECRETTK) as Token
+					
+					const user = await prisma.user.findUnique({ where: { id: decoded.id }})
+					
+					if (user) {
+						return { id: user.id, auth: true}
+					}
+
+					return { id: '', auth: false }
+				}
+				return { id: '', auth: false }
+			}
+		})
+	)
+
+	app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
+
+	httpServer.listen(PORT, () => {
+		console.log(`Server running in port http://localhost:${PORT}/graphql`)
+		console.log(`Web Sockets running in port ws://localhost:${PORT}/graphql`)
+	})
+}
+
+main()
