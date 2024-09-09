1b194f2c85323f21778fae1952318524c4edd4a7
Alexander Avendaño <avendano.ramirez@gmail.com>
2024-09-08 23:35:02 -0400

    Home modificado

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,19 +1,30 @@
+'use client'
 import Navbar from "@/ui/Navbar";
-import SessionProvider from "../lib/SessionProvider";
+import SessionProvider from "@/lib/SessionProvider";
 import { Toaster } from "react-hot-toast";
+import ModalDelete from "@/ui/ModalDelete";
+import { useModalStore } from "@/store/modalDelete";
 
 
 export default function Layout ({ children } : { children: React.ReactNode }) {
+  const { title, isOpen, closeModal, onConfirm } = useModalStore(state => state)
+
   return (
     <SessionProvider>
       <div className="flex">
-        <aside className="w-52">
+        <aside className="min-w-52">
           <Navbar/>
         </aside>
-        <main className="w-full flex flex-col justify-center items-center min-h-screen pr-10 py-5 gap-5">
+        <main className="w-full flex flex-col items-center min-h-screen pr-10 py-5 gap-5">
           {children}
         </main>
         <Toaster/>
+        <ModalDelete
+          isOpen={isOpen}
+          onClose={closeModal}
+          onConfirm={onConfirm}
+          title={title}
+        />
       </div>
     </SessionProvider>
   )
--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,41 +1,43 @@
 'use client'
-import React, { useState } from "react"
-import { useRouter } from "next/navigation"
-import Button from "@/components/ui/Button"
-import { PlusIcon, SearchIcon } from "@/icons/icons"
-import InputWithIcon from "@/components/ui/InputWithIcon"
-import CardPost from "@/components/ui/CardPost"
+import React from "react"
+import Button from "@/ui/Button"
+import { PlusIcon } from "@/icons"
+import CardPost from "@/ui/CardPost"
+import InputSearch from "@/ui/InputSearch"
+import Link from "next/link"
+import { useQuery } from "@apollo/client"
+import { GET_POSTS } from "../lib/graphql/posts"
+import { GetFollower, GetPosts } from "../lib/types/typesGraphql"
+import { useUserStore } from "../store/user"
+import { GET_FOLLOWERS } from "../lib/graphql/followers"
 
 function Home() {
-  const [searchText, setSearchText] = useState('')
-  const router = useRouter();
-
-  const handleSubmit = () =>{  
-    router.push(`/home/search?query=${encodeURIComponent(searchText)}`);
-  }
+	const { data: posts, loading, error } = useQuery<GetPosts>(GET_POSTS)
+	const { user } = useUserStore(state => state)
+	const { data: followers } = useQuery<GetFollower>(GET_FOLLOWERS, {
+    variables: {
+      idFollower: user.id
+    }
+  })
 
 	return (
 		<>
-			<section className='flex flex-col items-center w-full gap-5'>
-				<InputWithIcon
-					type='text'
-					placeholder='Buscar'
-          value={searchText}
-          onChange={e=>setSearchText(e.target.value)}
-					endContent={<SearchIcon className='size-5' />}
-          onSubmit={handleSubmit}
-				/>
-				<CardPost/>
-				<CardPost/>
-				<CardPost/>
-				<CardPost/>
+			<section className='flex flex-col h-full items-center w-full gap-5'>
+				<InputSearch/>
+				{followers?.getFollowers && (
+					posts?.getPosts.map(post => (
+						<CardPost key={post.id} post={post} following={followers?.getFollowers.some(follower => follower.idFollowing == post.user.id) || false}/>
+					))
+				)}
 			</section>
-			<Button
-				className='fixed bottom-5 right-5 px-3'
-				startContent={<PlusIcon />}
-			>
-				Crear
-			</Button>
+			<Link href={'/home/create'}>
+				<Button
+					className='fixed bottom-5 right-5 px-3'
+					startContent={<PlusIcon />}
+				>
+					Crear
+				</Button>
+			</Link>
 		</>
 	)
 }
