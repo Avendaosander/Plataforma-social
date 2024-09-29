f2738230a430b261653e07b3499b3e8b533ee591
Alexander Avendaño <avendano.ramirez@gmail.com>
2024-09-29 19:03:52 -0400

    Change in scrollbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en' className="scroll-smooth scrollbar-thin scrollbar-track-inherit scrollbar-thumb-seagreen-950">
+		<html lang='en' className="scroll-smooth scrollbar-thin scrollbar-track-inherit scrollbar-thumb-lime-400">
 			<body
 				className={clsx(
 					inter.className,
