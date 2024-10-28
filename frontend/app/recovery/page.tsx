8bd908df71bfa7183eb588c775d40ddfd940dc21
Alexander Avendaño <avendano.ramirez@gmail.com>
2024-10-28 11:09:58 -0400

    Add recovery

---
+++ frontend/app/recovery/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormRecovery from '../components/recovery/FormRecovery'
+
+function Login() {
+  return (
+    <main className='w-screen h-screen flex justify-center items-center gap-x-16'>
+      <div>
+        <FormRecovery/>
+      </div>
+    </main>
+  )
+}
+
+export default Login
